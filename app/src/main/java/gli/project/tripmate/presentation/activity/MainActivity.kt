814ca5cba0cd3b0bb3d18c9065476963df29d13b
farhan <farhanfathur73@gmail.com>
2025-03-13 09:27:21 +0700

    style: modify base theme, color, and typography

--- app/src/main/java/gli/project/tripmate/presentation/activity/MainActivity.kt
+++ app/src/main/java/gli/project/tripmate/presentation/activity/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import gli.project.tripmate.presentation.ui.theme.TripMateTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +13,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TripMateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MainApp {
+                Text(
+                    text = "Hello, World!"
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TripMateTheme {
-        Greeting("Android")
+fun MainApp(content: @Composable () -> Unit) {
+    TripMateTheme(
+        dynamicColor = false
+    ) {
+        content()
     }
 }
